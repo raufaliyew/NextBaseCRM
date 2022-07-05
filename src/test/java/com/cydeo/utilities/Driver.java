3e737e97e70a2fb9118dca071ae1a0edb64a1b6c
raufaliyew <rauf.aliyew@gmail.com>
2022-07-05 14:51:40 -0400

    Initial commit

---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,58 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver() {
+    }
+
+
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+
+
+
+        if (driverPool.get() == null) {
+
+            synchronized (Driver.class) {
+
+
+                String browserType = ConfReader.getProperty("browser");
+
+                switch (browserType) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        break;
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        driverPool.set(new FirefoxDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        break;
+                }
+            }
+        }
+
+        return driverPool.get();
+
+    }
+
+    // This method will make sure our driver value is always null after using quit() method
+    public static void closeDriver(){
+        if(driverPool.get() != null){
+            driverPool.get().quit(); // this line will terminate the existing driver session. with using this driver will not be even null
+            driverPool.remove();
+        }
+
+    }
+}
