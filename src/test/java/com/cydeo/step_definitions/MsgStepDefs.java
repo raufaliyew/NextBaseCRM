2ed0750ade53cc7c63558b204f028530e0cd0a88
ermek <sagynbekoff@icloud.com>
2022-07-08 13:09:11 -0500

    US2

---
+++ src/test/java/com/cydeo/step_definitions/MsgStepDefs.java
@@ -0,0 +1,88 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
+
+public class MsgStepDefs {
+
+    @Given("user is on main page")
+    public void user_is_on_main_page(){
+
+    }
+
+    @And("user click on message button")
+    public void userClickOnMessageButton() {
+    }
+
+    @And("user click send button")
+    public void userClickSendButton() {
+    }
+
+    @Then("user should see {string} msg")
+    public void userShouldSeeMsg(String arg0) {
+    }
+
+    @And("user input a msg in message feature")
+    public void userInputAMsgInMessageFeature() {
+    }
+
+    @Then("user should see msg on Activity Stream")
+    public void userShouldSeeMsgOnActivityStream() {
+    }
+
+    @And("user inputs a msg")
+    public void userInputsAMsg() {
+    }
+
+    @And("clicks save")
+    public void clicksSave() {
+    }
+
+    @And("clicks more button")
+    public void clicksMoreButton() {
+    }
+
+    @Then("should see delete option")
+    public void shouldSeeDeleteOption() {
+    }
+
+    @When("user clicks on delete button")
+    public void userClicksOnDeleteButton() {
+    }
+
+    @Then("msg should be canceled")
+    public void msgShouldBeCanceled() {
+    }
+
+    @Given("user is on  main page")
+    public void userIsOnMainPage() {
+    }
+
+    @When("user creates a message")
+    public void userCreatesAMessage() {
+    }
+
+    @And("user clicks attach a link option")
+    public void userClicksAttachALinkOption() {
+    }
+
+    @Then("user should see attach link pop up")
+    public void userShouldSeeAttachLinkPopUp() {
+    }
+
+    @When("user attach the link")
+    public void userAttachTheLink() {
+    }
+
+    @And("user clicks save button")
+    public void userClicksSaveButton() {
+    }
+
+    @Then("user should see the link attached to msg")
+    public void userShouldSeeTheLinkAttachedToMsg() {
+    }
+}
